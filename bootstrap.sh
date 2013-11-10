1bdf7abc4a799f11e4ded5f90929dca379708179
LRParser <lrparser@server.fake>
2013-11-10 11:33:23 -0500

    Adding play dependencies; run bootstrap to start web app

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,7 @@
 ant dist && cp dist/lib/jsan.jar activator-1.0.0/JStyloService/lib/ && cp lib/*.jar activator-1.0.0/JStyloService/lib/
 
 cp -r jsan_resources activator-1.0.0/JStyloService/
+
+cd ./activator-1.0.0/JStyloService
+
+../../play-2.2.0/play run
---
+++ play-2.2.0/jstylo-service/build.sbt
@@ -0,0 +1,11 @@
+name := "jstylo-service"
+
+version := "1.0-SNAPSHOT"
+
+libraryDependencies ++= Seq(
+  javaJdbc,
+  javaEbean,
+  cache
+)     
+
+play.Project.playJavaSettings
