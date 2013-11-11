b92753732103707f4e6ff6b305a9299b742a8319
LRParser <lrparser@server.fake>
2013-11-11 18:30:52 -0500

    Giving anonymouth service more time to compute results (the .get() method doesn't fully block the UI)

--- src/edu/drexel/psal/anonymouth/service/AnonymouthService.java
+++ src/edu/drexel/psal/anonymouth/service/AnonymouthService.java
@@ -11,12 +11,14 @@ import edu.drexel.psal.jstylo.generics.ProblemSet;
 
 public class AnonymouthService {
 
+
 	public static ArrayList<String[]> calculateAnonymouthSuggestions() throws Exception
 	{
 		ThePresident pres = new ThePresident();
-		ThePresident.num_Tagging_Threads = 1;
+	 	ThePresident.num_Tagging_Threads = 1;
 		pres.main.documentProcessor.process();
 		pres.main.documentProcessor.processing.get();
+		Thread.sleep(5000L);
 		Logger.logln("Done service call");
 		
 		return pres.main.wordSuggestionsDriver.getTopToRemove();
